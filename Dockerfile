866f472bd387630b7cc0695f374a48ca943028c3
Artem <artemdav1234@gmail.com>
2024-04-21 19:23:45 +0300

    initial

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:17-jdk-slim
-WORKDIR /app
-COPY /build/libs/*.jar app.jar
-CMD ["java", "-jar", "app.jar"]
--- src/main/java/org/example/views/Views.java
+++
@@ -1,6 +0,0 @@
-package org.example.views;
-
-public class Views {
-    public interface ViewForAccounts {}
-    public interface ViewForTasks {}
-}
