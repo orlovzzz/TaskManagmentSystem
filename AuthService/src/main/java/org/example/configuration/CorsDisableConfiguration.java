2ba9940ae3b4bdf3803c7e118d405ee47d246090
Artem <artemdav1234@gmail.com>
2024-05-19 21:11:13 +0300

    disable cors

--- AuthService/src/main/java/org/example/configuration/CorsDisableConfiguration.java
+++ AuthService/src/main/java/org/example/configuration/CorsDisableConfiguration.java
@@ -1,6 +1,7 @@
 package org.example.configuration;
 
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +9,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class CorsDisableConfiguration {
@@ -15,12 +22,28 @@ public class CorsDisableConfiguration {
     @Bean
     @SneakyThrows
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        return http.cors(Customizer.withDefaults()).csrf(AbstractHttpConfigurer::disable)
+        return http.csrf(csrf -> csrf.disable())
+                .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyRequest().permitAll()
                 )
-                .build();
+                .httpBasic(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final var config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setExposedHeaders(Arrays.asList("*"));
+        config.setAllowPrivateNetwork(true);
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+        final var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
