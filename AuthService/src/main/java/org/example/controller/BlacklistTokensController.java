f84ca4e282d4638fb63b39949ab0458d4e0ab247
orlovzzz <sedman142@gmail.com>
2024-08-09 22:31:29 +0300

    add /authApi

--- AuthService/src/main/java/org/example/controller/BlacklistTokensController.java
+++ AuthService/src/main/java/org/example/controller/BlacklistTokensController.java
@@ -9,13 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Blacklist Token Controller", description = "Add token in blacklist and check is exist")
-@Controller
+@RestController
+@RequestMapping("/authApi")
 @CrossOrigin
 public class BlacklistTokensController {
 
@@ -23,7 +21,7 @@ public class BlacklistTokensController {
     private BlacklistTokensService blacklistTokensService;
 
     @Operation(summary = "Add token in blacklist")
-    @PostMapping("/authApi/blacklist")
+    @PostMapping("/blacklist")
     public ResponseEntity addTokenInBlacklist(String token) {
         blacklistTokensService.addTokenInBlacklist(token);
         return new ResponseEntity<>(HttpStatus.OK);
